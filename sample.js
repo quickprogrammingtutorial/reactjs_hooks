f567b0d9afc586cfe68db0ee5ae5e786e7ceb3d3
quickprogrammingtutorial <89827496+quickprogrammingtutorial@users.noreply.github.com>
2021-09-06 20:06:38 +0800

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const sample = () => {
+  return (
+    <div className="panel">
+      <div><h3>[title]</h3></div>
+      <div>
+        {/* body */}
+      </div>
+      <div>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default sample;
